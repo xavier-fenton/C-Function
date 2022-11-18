a81029715a98339d9a5ef1ee27ac0e82ec226d87
Xavier Fenton <xavierfenton99@gmail.com>
2022-11-18 16:35:17 +1300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class SimpleMath
+{
+  public class Program
+  {
+      public static void Main(string [] args)
+      {
+
+        int R = 10;
+        
+        Console.WriteLine($"the area is: {CalculateArea(R)}"); 
+      }
+      public static double CalculateArea(int Radius)
+      {
+        double Pi = Math.PI;
+        double Area = Pi * Radius*Radius;
+        return Area;
+      }
+       
+  }
+}
+        // double firstSum = AddNumbers(12.7, 16.35);
+        // Console.WriteLine($"firstsum = {firstSum}");
+
